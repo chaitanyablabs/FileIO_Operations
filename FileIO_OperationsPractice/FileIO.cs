09f4a6abfe48f9f6d3e9bfafd10aa372856b3504
chaitanyablabs <chaitanya.jinka@bridgelabz.com>
2022-03-13 12:30:06 +0530

    [Chaitanya] Add: Added metod to check file existance

---
+++ FileIO_OperationsPractice/FileIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileIO_OperationsPractice
+{
+    public class FileIO
+    {
+        public bool IsExists(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File exists");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File does not exists");
+                return false;
+            }
+        }
+    }
+}
---
+++ FileIO_OperationsPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FileIO_OperationsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"G:\BridgeLabz\New folder\FileIO_Operations\FileIO_OperationsPractice\Files\Test.txt";
+
+            FileIO fileIO = new FileIO();
+
+            fileIO.IsExists(filePath);
+        }
+    }
+}
